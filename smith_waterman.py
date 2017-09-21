8cf7b4042c8cd79f20d8e21e19fd60dc291aba82
Carsen Ball <ballcarsen@gmail.com>
2017-09-21 12:35:32 -0600

    create alignment

--- smith_waterman.py
+++ smith_waterman.py
@@ -15,8 +15,29 @@ def calc_v(i,j):
     pass
 
 # Returns the correct alignment
-def create_alignment():
-    pass
+def create_alignment(s, t):
+    #Stacks to hold the characters for the alignment
+    s_prime = []
+    t_prime = []
+    score = global_max [0]
+    location = global_max[1]
+    while(score != 0):
+        direction = score_array[location[0]][location[1]][1]
+        if direction == 2:
+            s_prime.append(s[location[0]])
+            t_prime.append(t[location[1]])
+            location[0] = location[0] - 1
+            location[1] = location[1] - 1
+        elif direction == 1:
+            s_prime.append("_")
+            t_prime.append(t[location[1]])
+            location[1] = location[1] - 1
+        else:
+            t_prime.append("_")
+            s_prime.append(s[location[0]])
+            location[0] = location[0] - 1
+        score = score_array[location[0]][location[1]][0]
+
 
 def main():
     global global_max
@@ -29,7 +50,7 @@ def main():
     t_list = list(t)
 
     score_array = [[0] * len(t_list)] * len(s_list)
-
+    create_alignment(s_list, t_list)
     print(score_array)
 
 
