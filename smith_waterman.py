317b8d0938c0b99fcea7d5c0db38f301a3564893
BradWhite <white.brad17@gmail.com>
2017-09-21 12:04:14 -0600

    Shell program

--- smith_waterman.py
+++ smith_waterman.py
@@ -5,4 +5,31 @@ CSCI 451/551
 September 21, 2017
 '''
 
-# TODO: Some Stuff
+# Global variables
+global_max = [0, (0,0)]
+score_array = [[]]
+
+# Calculate the max score for a given index
+def calc_v(i,j):
+    pass
+
+# Returns the correct alignment
+def create_alignment():
+    pass
+
+def main():
+    global global_max
+    global score_array
+
+    s = "acaatcg"
+    t = "ctcatgc"
+
+    s_list = list(s)
+    t_list = list(t)
+
+    score_array = [[0] * len(t_list)] * len(s_list)
+
+    print(score_array)
+
+
+main()
